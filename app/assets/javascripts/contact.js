5f026f4db3b831276f74a9808eb959b6db283289
dami <damzcodes@gmail.com>
2015-08-24 18:22:46 +0100

    Contact form -wip

---
+++ app/assets/javascripts/contact.js
@@ -0,0 +1,27 @@
+var ContactForm = React.createClass({
+	getInitialState: function() {
+    return {data: []};
+  },
+
+  render: function() {
+  	return (
+  		// <div className="container">
+  			 <div className="row">
+          <div className="col-md-12">
+            <h1 className="text-center">Contact</h1>
+          </div>
+        </div>
+        <div className="row">
+        	<div className="col-md-12">
+        		<p className="text-center">
+        			You can contact me directly at me@damilolaodelola.com or you can fill out the contact form below.
+        		</p>
+        	</div>
+        </div>
+        </div>
+  		// </div>
+  	)
+
+  }
+
+})
